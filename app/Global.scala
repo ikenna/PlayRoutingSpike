bfb4b257b7fe510dbeabfe08db1db7b11c13a1ce
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2013-11-05 10:08:24 +0000

    Refactor

--- app/Global.scala
+++ app/Global.scala
@@ -5,12 +5,10 @@ object Global extends GlobalSettings{
 
 
   override def onRouteRequest(req: RequestHeader): Option[Handler] = {
-    if (req.host == "127.0.0.1:9000") {
-      foo.Routes.routes.lift(req)
-    } else if (req.host == "localhost:9000") {
-      bar.Routes.routes.lift(req)
-    } else {
-      super.onRouteRequest(req)
+    req.host match {
+      case "127.0.0.1:9000" =>  foo.Routes.routes.lift(req)
+      case "localhost:9000" =>  bar.Routes.routes.lift(req)
+      case _ =>  super.onRouteRequest(req)
     }
   }
 
